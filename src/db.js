ff2fc4c7ce9589ed44f0c07a05ab017cd024150b
adrian411997 <adrian.mart41197@gmail.com>
2022-12-12 17:41:02 -0300

    first-commit

---
+++ src/db.js
@@ -0,0 +1,54 @@
+require("dotenv").config();
+const { Sequelize, Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME } = process.env;
+let sequelize = new Sequelize({
+  database: DB_NAME,
+  dialect: "postgres",
+  host: DB_HOST,
+  port: 5432,
+  username: DB_USER,
+  password: DB_PASSWORD,
+  pool: { max: 3, min: 1, iddle: 100 },
+  dialectOptions: {
+    ssl: { require: false, rejectUnauthorized: false },
+    keepAlive: true,
+  },
+  ssl: false,
+});
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+fs.readdirSync(path.join(__dirname, "/Models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/Models", file)));
+  });
+
+modelDefiners.forEach((model) => model(sequelize));
+
+let entries = Object.entries(sequelize.models);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
+sequelize.models = Object.fromEntries(capsEntries);
+
+const { Products, Brands, Users } = sequelize.models;
+
+Brands.hasMany(Products, {
+  foreignKey: "brandId",
+});
+Products.belongsTo(Brands);
+
+module.exports = {
+  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  conn: sequelize,
+  Op,
+  // para importart la conexión { conn } = require('./db.js');
+};
